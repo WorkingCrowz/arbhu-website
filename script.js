a82b42c295dbfe449137e9934bbf7dc300b38354
WorkingCrowz <146631792+WorkingCrowz@users.noreply.github.com>
2025-08-29 12:28:37 +0530

    script.js

--- script.js
+++ script.js
@@ -25,13 +25,14 @@
 """
 
 # Write files
-with open('/mnt/data/index4_separated.html', 'w', encoding='utf-8') as f:
+with open('/mnt/data/index.html', 'w', encoding='utf-8') as f:
     f.write(html_content)
 
-with open('/mnt/data/styles.css', 'w', encoding='utf-8') as f:
+with open('/mnt/data/style.css', 'w', encoding='utf-8') as f:
     f.write(css_content)
 
 with open('/mnt/data/script.js', 'w', encoding='utf-8') as f:
     f.write(js_content)
 
-'/mnt/data/index4_separated.html', '/mnt/data/styles.css', '/mnt/data/script.js'
+'/mnt/data/index.html', '/mnt/data/style.css', '/mnt/data/script.js'
+
